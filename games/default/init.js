71e8f9955c8842ea3c4ae16b0fd4ae102d3c08ac
rarkenin <rarkenin@gmail.com>
2013-10-14 11:53:29 -0400

    Deduplicate script, edit good copy to match the file structure more. Add gitignore.

--- games/default/init.js
+++ games/default/init.js
@@ -1,2 +1,2 @@
-moss.registerNode("default:dirt", "Dirt", MapNode.getDefaultParams(), "Textures/default/dirt", false, 0);
+moss.registerNode("default:dirt", "Dirt", MapNode.getDefaultParams(), "default/dirt.png", false, 0);
 moss.registerNodeAlias("mg:ground", "default:dirt");
