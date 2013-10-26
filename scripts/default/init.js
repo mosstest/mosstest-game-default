0165613c8bab11e6c8ef24beb467dbdb630c97c1
rarkenin <rarkenin@gmail.com>
2013-10-26 16:18:51 -0400

    Update push script`

--- scripts/default/init.js
+++ scripts/default/init.js
@@ -1,5 +1,9 @@
 var dirt = moss.registerNode("default:dirt", "Dirt", new Packages.net.mosstest.scripting.DefaultNodeParams(), "default/dirt.png", false, 0);
 moss.registerNodeAlias("mg:dirt", "default:dirt");
+moss.registerNodeAlias("mg:stone", "default:dirt");
+moss.registerNodeAlias("mg:air", "default:dirt");
+moss.registerNodeAlias("mg:sand", "default:dirt");
 
 var grass = moss.registerNode("default:grass", "Grass",  new Packages.net.mosstest.scripting.DefaultNodeParams(), "default/grass.png", false, 0); 
 grass.dropItem = dirt.dropItem;
+moss.registerNodeAlias("mg:grass", "default:grass");
