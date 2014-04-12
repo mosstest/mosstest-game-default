34539ebf406a1b1bed747aefc26cf8f17af7cc4b
rarkenin <rarkenin@gmail.com>
2014-04-12 19:23:41 -0400

    update to new JS API

--- scripts/default/init.js
+++ scripts/default/init.js
@@ -1,12 +1,10 @@
-var dirtTex = new Packages.net.mosstest.scripting.CubeTextureSet();
-dirtTex.setAll("default/dirt.png");
-var dirt = moss.registerNode("default:dirt", "Dirt", new Packages.net.mosstest.scripting.DefaultNodeParams(), dirtTex, false, 0);
+
+var dirt = new Packages.net.mosstest.scripting.MapNode("default:dirt", "Dirt", "default/dirt.png", 0);
+moss.registerNode(dirt);
 moss.registerNodeAlias("mg:dirt", "default:dirt");
 moss.registerNodeAlias("mg:stone", "default:dirt");
 // moss.registerNodeAlias("mg:air", "default:dirt");
 moss.registerNodeAlias("mg:sand", "default:dirt");
-var grassTex = new Packages.net.mosstest.scripting.CubeTextureSet();
-grassTex.setAll("default/grass.png");
-var grass = moss.registerNode("default:grass", "Grass",  new Packages.net.mosstest.scripting.DefaultNodeParams(), grassTex, false, 0); 
+var grass = new Packages.net.mosstest.scripting.MapNode("default:grass", "Dirt", "default/grass.png", 0);
 grass.dropItem = dirt.dropItem;
 moss.registerNodeAlias("mg:grass", "default:grass");
