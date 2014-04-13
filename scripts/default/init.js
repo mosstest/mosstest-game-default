88ccbe2bba7da9f6bb366aa2c78f957f3e200cdf
rarkenin <rarkenin@gmail.com>
2014-04-13 14:50:34 -0400

    update script to new version of engine

--- scripts/default/init.js
+++ scripts/default/init.js
@@ -1,10 +1,10 @@
-
-var dirt = new Packages.net.mosstest.scripting.MapNode("default:dirt", "Dirt", "default/dirt.png", 0);
+importPackage(Packages.net.mosstest.scripting);
+var dirt = new MapNode("default:dirt", "Dirt", "default/dirt.png", 0);
 moss.registerNode(dirt);
 moss.registerNodeAlias("mg:dirt", "default:dirt");
 moss.registerNodeAlias("mg:stone", "default:dirt");
 // moss.registerNodeAlias("mg:air", "default:dirt");
 moss.registerNodeAlias("mg:sand", "default:dirt");
-var grass = new Packages.net.mosstest.scripting.MapNode("default:grass", "Dirt", "default/grass.png", 0);
+var grass = new MapNode("default:grass", "Dirt", "default/grass.png", 0);
 grass.dropItem = dirt.dropItem;
 moss.registerNodeAlias("mg:grass", "default:grass");
